6f44e7cbc59c89de9107d56db80b3a4e9f48bc20
Никита Зозуля <nik.zozulya.04@mail.ru>
2024-05-15 04:59:37 +0300

    Created consts for working

---
+++ pkg/context/consts.go
@@ -0,0 +1,31 @@
+package context
+
+const (
+	notOpenYetError       = "NotOpenYet"
+	youShallNotPassError  = "YouShallNotPass"
+	placeIsBusyError      = "PlaceIsBusy"
+	clientUnknownError    = "ClientUnknown"
+	iCanWaitNoLongerError = "ICanWaitNoLonger!"
+)
+
+const (
+	onClientComeStatus       = "1"
+	onClientSitAtTableStatus = "2"
+	onClientWaitStatus       = "3"
+	onClientGoAwayStatus     = "4"
+
+	systemOnClientGoAwayStatus     = "11"
+	systemOnClientSitAtTableStatus = "12"
+	systemOnErrorStatus            = "13"
+)
+
+const (
+	onClientComeBodyLen       = 1
+	onClientSitAtTableBodyLen = 2
+	onClientWaitBodyLen       = 1
+	onClientGoAwayBodyLen     = 1
+
+	systemOnClientGoAwayBodyLen     = 1
+	systemOnClientSitAtTableBodyLen = 2
+	systemOnErrorBodyLen            = 1
+)
