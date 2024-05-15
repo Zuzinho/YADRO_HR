78ddc6e05b6af1926a65d01ab15f905b159e81b9
Никита Зозуля <nik.zozulya.04@mail.ru>
2024-05-15 04:58:01 +0300

    Created type Duration and methods for it

---
+++ pkg/duration/duration.go
@@ -0,0 +1,67 @@
+package duration
+
+import (
+	"time"
+)
+
+const MinutesPerHour = 60
+
+type Duration struct {
+	Start time.Time
+	End   time.Time
+}
+
+func NewDuration(start time.Time) *Duration {
+	return &Duration{
+		Start: start,
+	}
+}
+
+func NewDurationWithEnd(start, end time.Time) *Duration {
+	return &Duration{
+		Start: start,
+		End:   end,
+	}
+}
+
+func (duration *Duration) Hours() int {
+	diff := duration.End.Sub(duration.Start)
+
+	minutes := int(diff.Minutes())
+
+	hours := minutes / MinutesPerHour
+
+	if minutes%MinutesPerHour > 0 {
+		return hours + 1
+	}
+
+	return hours
+}
+
+func (duration *Duration) Minutes() int {
+	diff := duration.End.Sub(duration.Start)
+
+	return int(diff.Minutes())
+}
+
+func (duration *Duration) InTime(t time.Time) bool {
+	return duration.Start.Before(t) && duration.End.After(t)
+}
+
+type Durations []*Duration
+
+func NewDurations() *Durations {
+	durs := make(Durations, 0)
+
+	return &durs
+}
+
+func NewDurationsWithLen(len int) *Durations {
+	durs := make(Durations, len)
+
+	return &durs
+}
+
+func (durations *Durations) Append(duration *Duration) {
+	*durations = append(*durations, duration)
+}
